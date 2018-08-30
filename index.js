30fa50b058ddf43219e0885e8ce44c59c9518b0c
Keith Simon <keith@locotec.co.za>
2018-08-30 05:11:41 +0200

    Improved command processing

--- index.js
+++ index.js
@@ -19,9 +19,55 @@ const USER_FILE_PATH = process.env.USERS_FILE || 'users.json';
 const QUESTION_FILE_PATH = process.env.QUESTIONS_FILE || 'questions.json';
 const ANSWER_FILE_PATH = process.env.ANSWERS_FILE || 'answers.txt';
 
-const COMMAND_JOIN = process.env.COMMAND_JOIN;
-const COMMAND_START = process.env.COMMAND_START;
-const COMMAND_ADD_QUESTION = process.env.COMMAND_ADD_QUESTION;
+
+const COMMAND_INITIALIZER = process.env.COMMAND_INITIALIZER || '!q-bot';
+
+const COMMAND_HELP = process.env.COMMAND_HELP || 'help';
+const COMMAND_JOIN = process.env.COMMAND_JOIN || 'join';
+const COMMAND_LEAVE = process.env.COMMAND_LEAVE || 'leave';
+const ADMIN_COMMAND_INVITE = process.env.ADMIN_COMMAND_INVITE || 'invite';
+const ADMIN_COMMAND_KICK = process.env.ADMIN_COMMAND_KICK || 'kick';
+const ADMIN_COMMAND_START = process.env.ADMIN_COMMAND_START || 'start';
+const ADMIN_COMMAND_ADD_QUESTION = process.env.ADMIN_COMMAND_ADD_QUESTION || 'add-question';
+
+const COMMANDS = {
+    [COMMAND_HELP]: {
+        adminOnly: false,
+        hasParams: false,
+        func: showHelp
+    },
+    [COMMAND_JOIN]: {
+        adminOnly: false,
+        hasParams: false,
+        func: userJoin
+    },
+    [COMMAND_LEAVE]: {
+        adminOnly: false,
+        hasParams: false,
+        func: userLeave
+    },
+    [ADMIN_COMMAND_INVITE]: {
+        adminOnly: true,
+        hasParams: 'nick',
+        func: userJoin
+    },
+    [ADMIN_COMMAND_KICK]: {
+        adminOnly: true,
+        hasParams: 'nick',
+        func: userLeave
+    },
+    [ADMIN_COMMAND_START]: {
+        adminOnly: true,
+        hasParams: 'nick (optional)',
+        func: startQuestions
+    },
+    [ADMIN_COMMAND_ADD_QUESTION]: {
+        adminOnly: true,
+        hasParams: 'question',
+        func: addQuestion
+    }
+
+};
 
 if (DEBUG) {
     console.log(`Connecting to ${IRC_SERVER} as ${IRC_NICK} with${IRC_PASS ? ' password' : 'out a password'}`);
@@ -32,11 +78,10 @@ if (!IRC_SERVER || !IRC_NICK || !IRC_ADMIN_NICK || !IRC_PUBLIC_CHANNEL) {
     console.error('Missing IRC_SERVER or IRC_NICK or IRC_ADMIN_NICK or IRC_PUBLIC_CHANNEL');
     process.exit(1);
 }
+
 const users = readUsers();
 const questions = readQuestions();
-
-
-let responses = {};
+const responses = {};
 
 const client = new irc.Client(IRC_SERVER, IRC_NICK, {
     userName: IRC_NICK,
@@ -51,43 +96,68 @@ const client = new irc.Client(IRC_SERVER, IRC_NICK, {
 client.addListener('message', function (from, to, message) {
     // console.log(from + ' => ' + to + ': ' + message);
 
-    //Admin only commands
-    if (from === IRC_ADMIN_NICK && message[0] === '!') {
-        //Get answers from all users
-        if (message.indexOf(COMMAND_START) === 0) {
-            let startCommands = message.split(' ');
-            //There is only the command, so run it for all the users
-            if (startCommands.length === 1) {
-                startQuestions(users);
-            } else {
-                //Run it for these specific users
-                startCommands.splice(0,1);
-                startQuestions(startCommands);
-            }
-        }
-
-        if (message.indexOf(`${COMMAND_ADD_QUESTION} `) === 0) {
-            //Add a new question
-            storeQuestion(message.replace(`${COMMAND_ADD_QUESTION} `, ''));
-        }
-
-    } else {
-        //Public commands
-
-        if (
-            to === process.env.IRC_NICK
-            && users.indexOf(from) > -1
-            && responses.hasOwnProperty(from)) {
-            //This is a message to the bot from someone in our user list
-            addAnswer(from, message);
-        } else if (message === COMMAND_JOIN) {
-            //Someone has sent the join command
-            addRemoveUser(from);
-        }
+    //Check if the command initializer has been said
+    if (message[0] === '!' && message.substr(0, COMMAND_INITIALIZER.length) === COMMAND_INITIALIZER) {
+        processCommands(from, to, message);
+    } else if (
+        to === IRC_NICK
+        && users.indexOf(from) > -1
+        && responses.hasOwnProperty(from)) {
+        //Is it a direct message and the from user is in our list of users and we are expecting a response
+        //This is a message to the bot from someone in our user list
+        addAnswer(from, message);
     }
 
 
 });
+
+function processCommands(from, to, messageString) {
+    //We must execute a command
+    let commandParts = messageString.split(' ');
+    //Remove the command initializer
+    commandParts.splice(0, 1);
+    if (commandParts.length === 0) {
+        //No commands added
+        client.say(from, 'Invalid command');
+        showHelp(from);
+        return;
+    }
+
+    const command = commandParts.shift();
+    const params = commandParts;
+
+    //Check if the command exists.
+    if (!COMMANDS.hasOwnProperty(command)) {
+        client.say(from, 'Invalid command');
+        showHelp(from);
+        return;
+    }
+
+    //Check if only admins can run it
+    const commandIsAdmin = COMMANDS[command].adminOnly;
+    if (commandIsAdmin && from !== IRC_ADMIN_NICK) {
+        client.say(from, 'You do not have permission to run that command');
+        return;
+    }
+
+    //Run the command
+    COMMANDS[command].func(from, to, COMMANDS[command].hasParams ? params : null);
+
+}
+
+function showHelp(user) {
+    let helpMessage = `${BOT_NAME} is a questionnaire bot, it will PM you a set of questions and broadcast your answers back to ${IRC_PUBLIC_CHANNEL}\n`;
+    const isAdmin = user === IRC_ADMIN_NICK;
+    for (let commandText in COMMANDS) {
+        let command = COMMANDS[commandText];
+        if (!command.adminOnly) {
+            helpMessage += `${COMMAND_INITIALIZER} ${commandText} ${command.hasParams || ''}\n`;
+        } else if (command.adminOnly && isAdmin) {
+            helpMessage += `${COMMAND_INITIALIZER} ${commandText} ${command.hasParams || ''}\n`;
+        }
+    }
+    client.say(user, helpMessage);
+}
 
 client.addListener('error', function (message) {
     console.log('error: ', message);
@@ -97,7 +167,7 @@ client.addListener('error', function (message) {
         try {
 
             delete responses[user];
-        }catch(e) {
+        } catch (e) {
 
         }
     }
@@ -108,17 +178,20 @@ client.connect(0, () => {
 });
 
 
-function startQuestions(users) {
-    responses = {};
-    users.forEach(user => {
+function startQuestions(from, to, params) {
+
+    let usersToPoll = users;
+    if (params.length > 0) {
+        usersToPoll = params;
+    }
+
+    usersToPoll.forEach(user => {
         startQuestionsForUser(user);
     });
 }
 
 function startQuestionsForUser(user) {
-    if (!responses.hasOwnProperty(user)) {
-        responses[user] = {};
-    }
+    responses[user] = {};
     askQuestion(user, questions[0])
 }
 
@@ -160,29 +233,84 @@ function postToChannel(user) {
     });
 }
 
-function addRemoveUser(user) {
-    let userIndex = users.indexOf(user);
-    if (userIndex > -1) {
-        users.splice(userIndex, 1);
-    } else {
-        users.push(user);
+
+function userLeave(from, to, params) {
+    let userToLeave = from;
+    if (params) {
+        userToLeave = params.shift().trim();
     }
-    fs.writeFile(USER_FILE_PATH, JSON.stringify(users), function (err) {
+    let userIndex = users.indexOf(userToLeave);
+    if (userIndex === -1 || !userToLeave) {
+        client.say(from, `${userToLeave} does not exist`);
+        return;
+    }
+
+    users.splice(userIndex, 1);
+
+    storeUsers(users, (err) => {
         if (err) {
-            console.error(err);
-        }
-        if (userIndex > -1) {
-            client.say(user, 'You have been removed from the daily dev standup');
+            client.say(from, 'There was an error storing the user');
         } else {
-            client.say(user, 'You have been added to the daily dev standup');
+            let addressee = 'You have';
+            if (from !== userToLeave) {
+                addressee = userToLeave + ' has';
+            }
+            client.say(from, `${addressee} been removed from ${BOT_NAME}`);
         }
     });
 }
 
+
+function userJoin(from, to, params) {
+    let userToJoin = from;
+    if (params) {
+        userToJoin = params.shift().trim();
+    }
+    let userIndex = users.indexOf(userToJoin);
+    if (userIndex > -1 || !userToJoin) {
+        client.say(from, `${userToJoin} has already joined`);
+        return;
+    }
+    users.push(userToJoin);
+    storeUsers(users, (err) => {
+        if (err) {
+            client.say(from, 'There was an error storing the user');
+        } else {
+            let addressee = 'You have';
+            if (from !== userToJoin) {
+                addressee = userToJoin + ' has';
+            }
+            client.say(from, `${addressee} been added to ${BOT_NAME}`);
+        }
+    });
+
+}
+
+function storeUsers(users, cb) {
+    fs.writeFile(USER_FILE_PATH, JSON.stringify(users), function (err) {
+        if (err) {
+            console.error(err);
+        }
+        if (cb) {
+            cb(err);
+        }
+    });
+}
+
+
+function addQuestion(from, to, params) {
+    let question = params.join(' ');
+    storeQuestion(question);
+}
+
 function storeQuestion(question) {
+    if (question.trim() === '') {
+        return false;
+    }
+
     questions.push(question);
 
-    fs.writeFile(QUESTION_FILE_PATH,  JSON.stringify(questions), function (err) {
+    fs.writeFile(QUESTION_FILE_PATH, JSON.stringify(questions), function (err) {
         if (err) {
             console.error(err);
         }
@@ -200,6 +328,7 @@ function storeAnswer(user) {
         }
     });
 }
+
 function readQuestions() {
     return readJsonFile(QUESTION_FILE_PATH, [])
 }
